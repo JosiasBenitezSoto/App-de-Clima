c3769f79347fd1bd7d101802a39fe7a70e7a09ec
Josias Benitez <142059326+JosiasBenitezSoto@users.noreply.github.com>
2024-07-22 16:41:41 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    private static CosumoAPI CosumoAPI;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo, ¿Como estas? ");
+
+    }
+}
